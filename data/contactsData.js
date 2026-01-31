7f0c2be9b68566f69481820f1a7d4e045c5b5999
Nihab Ali <nihabali01@gmail.com>
2026-02-01 00:11:20 +0600

    Update contactsData.js

--- data/contactsData.js
+++ data/contactsData.js
@@ -3,7 +3,7 @@ const contactsData = [
         id: 1,
         platform: "Discord",
         handle: "nihabali",
-        url: "https://discord.com/users/your-user-id",
+        url: "https://discord.com/users/1403726933069660240",
         icon: "discord" 
     },
     {
