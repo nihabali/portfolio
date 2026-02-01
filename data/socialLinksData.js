b2f3d2354f99a0219cccd20d7c73bceb19b9c540
Nihab Ali <nihabali01@gmail.com>
2026-02-01 22:15:19 +0600

    Update social links icons and add new entries

--- data/socialLinksData.js
+++ data/socialLinksData.js
@@ -1,10 +1,10 @@
 const socialLinksData = [
     { name: "Anilist", icon: "account_circle", url: "https://anilist.co/user/7036839" },
     { name: "Instagram", icon: "photo_camera", url: "https://www.instagram.com/nihabali01" },
-    { name: "Twitter", icon: "tag", url: "https://x.com/nihabali01" },
+    { name: "Twitter", icon: "alternate_email", url: "https://x.com/nihabali01" },
     { name: "GitHub", icon: "code", url: "https://github.com/nihabali" },
-    { name: "TikTok", icon: "account_circle", url: "https://www.tiktok.com/@nihab_ali" },
-    { name: "Facebook", icon: "account_circle", url: "https://web.facebook.com/nihabali01" },
-    { name: "Mastodon", icon: "account_circle", url: "https://mastodon.social/@nihabali" },
-    { name: "Blue Sky", icon: "account_circle", url: "https://bsky.app/profile/nihabali.bsky.social" }
+    { name: "Blue Sky", icon: "verified", url: "https://bsky.app/profile/nihabali.bsky.social" },
+    { name: "TikTok", icon: "tag", url: "https://www.tiktok.com/@nihab_ali" },
+    { name: "Facebook", icon: "tag", url: "https://web.facebook.com/nihabali01" },
+    { name: "Mastodon", icon: "tag", url: "https://mastodon.social/@nihabali" }
 ];
