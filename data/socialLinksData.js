94ebfc6a8d9dbabccf19fcd373d90866ad729490
Nihab Ali <nihabali01@gmail.com>
2026-02-01 22:10:02 +0600

    Update socialLinksData.js

--- data/socialLinksData.js
+++ data/socialLinksData.js
@@ -3,8 +3,8 @@ const socialLinksData = [
     { name: "Instagram", icon: "photo_camera", url: "https://www.instagram.com/nihabali01" },
     { name: "Twitter", icon: "tag", url: "https://x.com/nihabali01" },
     { name: "GitHub", icon: "code", url: "https://github.com/nihabali" },
-    { name: "TikTok", icon: "link", url: "https://www.tiktok.com/@nihab_ali" },
-    { name: "Facebook", icon: "link", url: "https://web.facebook.com/nihabali01" },
-    { name: "Mastodon", icon: "link", url: "https://mastodon.social/@nihabali" },
-    { name: "Blue Sky", icon: "link", url: "https://bsky.app/profile/nihabali.bsky.social" }
+    { name: "TikTok", icon: "account_circle", url: "https://www.tiktok.com/@nihab_ali" },
+    { name: "Facebook", icon: "account_circle", url: "https://web.facebook.com/nihabali01" },
+    { name: "Mastodon", icon: "account_circle", url: "https://mastodon.social/@nihabali" },
+    { name: "Blue Sky", icon: "account_circle", url: "https://bsky.app/profile/nihabali.bsky.social" }
 ];
