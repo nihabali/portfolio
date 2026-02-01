70e6696e7ba3c92a68538f35963a56428ab6759c
Nihab Ali <nihabali01@gmail.com>
2026-02-01 22:37:55 +0600

    Update profileData.js

--- data/profileData.js
+++ data/profileData.js
@@ -1,6 +1,6 @@
 const profileData = {
     name: "ɴɪʜᴀʙ ᴀʟɪ",
-    tagline: "Digital Artist | Hardcore Gamer",
+    tagline: "Open Source & Privacy believer",
     bio: "Welcome! I love creating digital art and ranking up in competitive shooters.",
     avatarUrl: "image/profile/profile.webp",
     coverUrl: "image/profile/cover.webp"
