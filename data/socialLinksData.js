8bd9be43b9e5238e0e555367b5c8711a42e65210
Nihab Ali <nihabali01@gmail.com>
2026-02-01 22:25:04 +0600

    Update socialLinksData.js

--- data/socialLinksData.js
+++ data/socialLinksData.js
@@ -1,10 +1,10 @@
 const socialLinksData = [
-    { name: "Anilist", icon: "account_circle", url: "https://anilist.co/user/7036839" },
+    { name: "Anilist", icon: "view_object_track", url: "https://anilist.co/user/7036839" },
     { name: "Instagram", icon: "photo_camera", url: "https://www.instagram.com/nihabali01" },
     { name: "Twitter", icon: "alternate_email", url: "https://x.com/nihabali01" },
     { name: "GitHub", icon: "code", url: "https://github.com/nihabali" },
     { name: "Blue Sky", icon: "verified", url: "https://bsky.app/profile/nihabali.bsky.social" },
-    { name: "TikTok", icon: "tag", url: "https://www.tiktok.com/@nihab_ali" },
-    { name: "Facebook", icon: "tag", url: "https://web.facebook.com/nihabali01" },
-    { name: "Mastodon", icon: "tag", url: "https://mastodon.social/@nihabali" }
+    { name: "TikTok", icon: "music_note", url: "https://www.tiktok.com/@nihab_ali" },
+    { name: "Facebook", icon: "account_circle", url: "https://web.facebook.com/nihabali01" },
+    { name: "Mastodon", icon: "communities", url: "https://mastodon.social/@nihabali" }
 ];
