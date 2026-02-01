230d2075267198440037ab5d9e8c5a837038ca35
Nihab Ali <nihabali01@gmail.com>
2026-02-01 21:18:10 +0600

    Update socialLinksData.js

--- data/socialLinksData.js
+++ data/socialLinksData.js
@@ -1,6 +1,10 @@
 const socialLinksData = [
-    { name: "Instagram", icon: "photo_camera", url: "#" },
-    { name: "Twitter", icon: "tag", url: "#" },
-    { name: "GitHub", icon: "code", url: "#" },
-    { name: "LinkedIn", icon: "work", url: "#" }
+    { name: "Anilist", icon: "link", url: "https://anilist.co/user/7036839" },
+    { name: "Instagram", icon: "photo_camera", url: "https://www.instagram.com/nihabali01" },
+    { name: "Twitter", icon: "tag", url: "https://x.com/nihabali01" },
+    { name: "GitHub", icon: "code", url: "https://github.com/nihabali" },
+    { name: "TikTok", icon: "link", url: "https://www.tiktok.com/@nihab_ali" },
+    { name: "Facebook", icon: "link", url: "https://web.facebook.com/nihabali01" },
+    { name: "Mastodon", icon: "link", url: "https://mastodon.social/@nihabali" },
+    { name: "Blue Sky", icon: "link", url: "https://bsky.app/profile/nihabali.bsky.social" }
 ];
